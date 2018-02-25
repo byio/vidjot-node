ed63e9ab7bea7967143d26585fd6825419c7fcd9
byio <ben@benyang.io>
2018-02-25 19:33:39 +0800

    added server side and client side validation for add idea form

--- controllers/ideaController.js
+++ controllers/ideaController.js
@@ -1,3 +1,18 @@
 exports.renderAddIdeaForm = (req, res) => {
   res.render('ideas/add');
 };
+
+exports.handleAddIdea = (req, res) => {
+  let errors = [];
+  if (!req.body.title) errors.push({ text: 'Please add a title.' });
+  if (!req.body.details) errors.push({ text: 'Please add a description.' });
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send('passed');
+  }
+};
--- routes/ideas.js
+++ routes/ideas.js
@@ -7,5 +7,6 @@ const idea_controller = require('../controllers/ideaController');
 
 // /ideas Routes
 router.get('/add', idea_controller.renderAddIdeaForm);
+router.post('/', idea_controller.handleAddIdea);
 
 module.exports = router;
