f89ce46d4e7bf88675073d50e0586df705ba80ad
byio <ben@benyang.io>
2018-02-27 23:03:57 +0800

    set up validation for users registration form

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,3 +8,24 @@ exports.renderLoginForm = (req, res) => {
 exports.renderRegisterForm = (req, res) => {
   res.render('users/register');
 };
+
+exports.handleUsersRegistration = (req, res) => {
+  let errors = [];
+  if (req.body.password !== req.body.password2) {
+    errors.push({ text: 'Passwords do not match.' });
+  }
+  if (req.body.password.length < 4) {
+    errors.push({ text: 'Password must be at least 4 characters long.' });
+  }
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send('passed');
+  }
+};
