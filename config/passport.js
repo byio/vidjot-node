627d04878d4319f157ae5d5d39acfce6107bab7b
byio <ben@benyang.io>
2018-02-28 22:28:11 +0800

    match email to user in database, and match password with bcrypt

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,27 @@
 const LocalStrategy = require('passport-local').Strategy;
-const bycrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 // Load User Model
 const User = require('../models/User');
 
 module.exports = (passport) => {
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    console.log(email);
+    // find user by email
+    User.findOne({ email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'No such user.' });
+          } else {
+            // match password
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+              if (err) throw err;
+              if (isMatch) {
+                return done(null, user);
+              } else {
+                return done(null, false, { message: 'Invalid Password.' });
+              }
+            });
+          }
+        });
   }));
 };
