9d47a9b6b3c37ba34e2da5ea6ccc977cdf5ba435
byio <ben@benyang.io>
2018-03-01 16:24:54 +0800

    create ensureLoggedOut authentication helper to protect routes that can only be accessed when users are logged out

--- helpers/auth.js
+++ helpers/auth.js
@@ -5,3 +5,11 @@ exports.ensureAuthenticated = (req, res, next) => {
   req.flash('error_msg', 'You need to login to access this page.');
   res.redirect('/users/login');
 };
+
+exports.ensureLoggedOut = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  req.flash('error_msg', 'You are already logged in.');
+  res.redirect('/ideas');
+};
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const { ensureAuthenticated } = require('../helpers/auth');
+const { ensureAuthenticated, ensureLoggedOut } = require('../helpers/auth');
 
 const router = express.Router();
 
 // Load User Controller
 const user_controller = require('../controllers/userController');
 
-router.get('/login', user_controller.renderLoginForm);
-router.get('/register', user_controller.renderRegisterForm);
-router.get('/logout', user_controller.handleUsersLogout);
+router.get('/login', ensureLoggedOut, user_controller.renderLoginForm);
+router.get('/register', ensureLoggedOut, user_controller.renderRegisterForm);
+router.get('/logout', ensureAuthenticated, user_controller.handleUsersLogout);
 router.post('/login', user_controller.handleUsersLogin);
 router.post('/register', user_controller.handleUsersRegistration);
 
