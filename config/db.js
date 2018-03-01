fdeb2368d0cf6fd12d001dac8e6d25a21df74117
byio <ben@benyang.io>
2018-03-01 23:55:17 +0800

    use temporary mlad credentials to test request timeout post request to mlad from heroku

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = { mongoURI: process.env.MONGOLAB_URI }
+  module.exports = { mongoURI: "mongodb://benyang:password@ds133017.mlab.com:33017/vidjot-node-dev" }
 } else {
   module.exports = { mongoURI: process.env.DB_LOCAL }
 }
